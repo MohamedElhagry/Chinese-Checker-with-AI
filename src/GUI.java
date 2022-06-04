f33df9789a84e5cf7cbfe183b291839f996a8530
Abdallah El belkasy <59124608+Abdaishere@users.noreply.github.com>
2022-06-04 16:00:31 +0200

    gui working yay

--- src/GUI.java
+++ src/GUI.java
@@ -1,20 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.util.concurrent.CountDownLatch;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
-public class GUI extends JFrame implements MouseListener
-{
+public class GUI extends JFrame implements MouseListener {
     public static int clickCounter = 0;
-    public static boolean guiPlayerFinished = false;
-    public static CountDownLatch latch = new CountDownLatch(1);
-    public static int[] clicks = {0,0,0,0};
+    public static int[] clicks = {0, 0, 0, 0};
     public static final int SCREEN_WIDTH = 1000;
     public static final int SCREEN_HEIGHT = 680;
     public static final int CIRCLE_SIZE = 40;
+    ArrayList<Integer> moves;
 
-    GUI()
-    {
+    GUI() {
         this.setResizable(false);
         this.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
         this.getContentPane().setBackground(Color.DARK_GRAY);
@@ -22,107 +21,119 @@ public class GUI extends JFrame implements MouseListener
         this.setLayout(null);
         this.setVisible(true);
         clickCounter = 0;
-        guiPlayerFinished =false;
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
                 Point lastClick = new Point(mouseEvent.getX(), mouseEvent.getY());
                 Point actualPoint = new Point(invMap(lastClick));
-                System.out.println("Clicked on "+actualPoint.x+", "+actualPoint.y);
-                if(clickCounter == 0)
-                {
-                    if (latch == null) latch = new CountDownLatch(1);
-                    guiPlayerFinished = false;
+                System.out.println("Clicked on " + actualPoint.x + ", " + actualPoint.y);
+                if (Utilities.find(StateManager.curr.redBalls, Graph.map(actualPoint.x, actualPoint.y))) {
+
                     clicks[0] = actualPoint.x;
                     clicks[1] = actualPoint.y;
                     clickCounter = 1;
-                }
-                else
-                {
+
+                    ArrayList<StateManager.Move> allmoves = GameManager.getPlayerMove();
+                    moves = new ArrayList<>();
+
+                    for (StateManager.Move m : allmoves) {
+                        if (m.x1 == clicks[0] && m.y1 == clicks[1]) moves.add(Graph.map(m.x2, m.y2));
+                    }
+
+                    reDraw();
+                } else if (find(moves, Graph.map(actualPoint.x, actualPoint.y))) {
+
                     clicks[2] = actualPoint.x;
                     clicks[3] = actualPoint.y;
                     clickCounter = 0;
+
+                    StateManager.curr.doMove(clicks[0], clicks[1], clicks[2], clicks[3]);
+                    moves = new ArrayList<>();
+
+                    reDraw();
+
+
+                    // AI plays
+                    GameManager.checkwinning();
+                    GameManager.switchTurn();
+                    reDraw();
                 }
-
-
             }
         });
 
 
     }
 
-    public void reDraw()
-    {
+
+    public void reDraw() {
         this.repaint();
     }
 
 
-    public void paint (Graphics g)
-    {
+    public void paint(Graphics g) {
         super.paint(g);
         Graphics2D graphics2D = (Graphics2D) g;
 
 
         //Draw board
-        for (Graph.Node node: Graph.nodes)
-        {
+        for (Graph.Node node : Graph.nodes) {
             g.setColor(Color.WHITE);
-            if(node == null) continue;
+            if (node == null) continue;
             Point position = new Point(map(Graph.getCol(node.num), Graph.getRow(node.num)));
-            if(Utilities.find(StateManager.curr.redBalls, node.num))
-                g.setColor(Color.RED);
-            else if(Utilities.find(StateManager.curr.blueBalls, node.num))
-                g.setColor(Color.BLUE);
+            if (Utilities.find(StateManager.curr.redBalls, node.num)) g.setColor(Color.RED);
+            else if (Utilities.find(StateManager.curr.blueBalls, node.num)) g.setColor(Color.BLUE);
+            else if (find(moves, node.num)) g.setColor(Color.YELLOW);
 
-            g.fillOval(position.x,position.y,CIRCLE_SIZE,CIRCLE_SIZE);
+            g.fillOval(position.x, position.y, CIRCLE_SIZE, CIRCLE_SIZE);
         }
     }
 
+    boolean find(ArrayList<Integer> moves, int num) {
+        if (moves == null)
+            return false;
+        for (Integer m : moves) {
+            if (m == num)
+                return true;
+        }
+        return false;
+    }
 
-    public Point invMap(Point point)
-    {
-        int newX = ((point.x / (SCREEN_WIDTH / 25)) );
-        int newY = (int) (((point.y - 35.0)/0.95) * (17.0/SCREEN_HEIGHT));
+    public Point invMap(Point point) {
+        int newX = ((point.x / (SCREEN_WIDTH / 25)));
+        int newY = (int) (((point.y - 35.0) / 0.95) * (17.0 / SCREEN_HEIGHT));
         return new Point(newY, newX);
     }
 
 
-    public Point map(int x, int y)
-    {
-        int newX = (int) (((SCREEN_WIDTH / 25) * x) );
-        int newY = (int) (((SCREEN_HEIGHT / 17) * y) * (0.95)+35);
+    public Point map(int x, int y) {
+        int newX = (int) (((SCREEN_WIDTH / 25) * x));
+        int newY = (int) (((SCREEN_HEIGHT / 17) * y) * (0.95) + 35);
         return new Point(newX, newY);
     }
 
     @Override
-    public void mouseClicked(MouseEvent mouseEvent)
-    {
-
+    public void mouseClicked(MouseEvent mouseEvent) {
 
 
     }
 
     @Override
-    public void mousePressed(MouseEvent mouseEvent)
-    {
+    public void mousePressed(MouseEvent mouseEvent) {
 
     }
 
     @Override
-    public void mouseReleased(MouseEvent mouseEvent)
-    {
+    public void mouseReleased(MouseEvent mouseEvent) {
 
     }
 
     @Override
-    public void mouseEntered(MouseEvent mouseEvent)
-    {
+    public void mouseEntered(MouseEvent mouseEvent) {
 
     }
 
     @Override
-    public void mouseExited(MouseEvent mouseEvent)
-    {
+    public void mouseExited(MouseEvent mouseEvent) {
 
     }
 }
--- src/GameManager.java
+++ src/GameManager.java
@@ -1,77 +1,42 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class GameManager {
+    GUI gui;
+    static MiniMax intelligence;
+    static int depth;
 
-    void startGame()
-    {
-        State state = State.getInitialState();
-        play(state,3);
+    void startGame() {
+        StateManager.curr = State.getInitialState();
+
+        play(3);
     }
 
-    void play(State state, int depth)
-    {
-        boolean AITurn = false;
-        MiniMax intelligence = new MiniMax();
+    public static ArrayList<StateManager.Move> getPlayerMove() {
+        ArrayList<StateManager.Move> possibleMoves = StateManager.getValidMoves(StateManager.curr);
+        return possibleMoves;
+    }
+
+    void play(int d) {
+        this.depth = d;
+        intelligence = new MiniMax();
 
         System.out.println("Please enter your move like so : ");
         System.out.println("enter the row num and col num of the piece you want to move");
         System.out.println("then enter the row num and col num of the piece you want to move to");
 
-        while(state.isWin() == 0)
-        {
-            if(AITurn)
-            {
-                state = intelligence.miniMax(state, true, depth);
-                AITurn = false;
-            }
-            else{
-                state.printState();
-                System.out.println("Please enter your move");
-
-
-                int x1,y1,x2,y2;
-                Scanner sc = new Scanner(System.in);
-                x1 = sc.nextInt();
-                y1 = sc.nextInt();
-                x2 = sc.nextInt();
-                y2 = sc.nextInt();
-
-                ArrayList<StateManager.Move> possibleMoves = StateManager.getValidMoves(state, false);
-                boolean validMove = false;
-                for(StateManager.Move move : possibleMoves)
-                {
-                    if(x1 == move.x1 && y1 == move.y1 && x2 == move.x2 && y2 == move.y2)
-                    {
-                        validMove = true;
-                        break;
-                    }
-                }
-                for(StateManager.Move move : possibleMoves)
-                {
-                    move.print();
-                }
-
-
-                if(validMove){
-                    state.doMove(x1,y1,x2,y2);
-                    AITurn = true;
-                }
-                else{
-                    System.out.println("Invalid move, please enter another move");
-                }
-
-                //make player move
-            }
-        }
-
-        if(state.isWin() == -1)
-            System.out.println("Congratulations, the player won!");
-        else
-            System.out.println("The AI won this time.");
-
     }
 
+    static void switchTurn() {
+        StateManager.curr = intelligence.miniMax(StateManager.curr, true, depth);
+    }
+
+    static void checkwinning() {
+        int status = StateManager.curr.isWin();
+        if (status == -1)
+            System.out.println("Congratulations, the player won!");
+        else if (status == 1)
+            System.out.println("The AI won this time.");
+    }
 
     public static void main(String[] args) {
         new Graph();
